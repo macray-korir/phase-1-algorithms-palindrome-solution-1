0a1f2854d29e1a286ac7c29ba5ea99a9defab481
macray <macraykorir254@gmail.com>
2023-07-10 14:10:48 +0300

    Completed

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Convert the word to lowercase for case-insensitive comparison
+  const lowerCaseWord = word.toLowerCase();
+
+  // Initialize pointers for the start and end of the word
+  let start = 0;
+  let end = lowerCaseWord.length - 1;
+
+  // Loop until the pointers meet or cross each other
+  while (start < end) {
+    // Compare the characters at the start and end positions
+    if (lowerCaseWord[start] !== lowerCaseWord[end]) {
+      return false; // Characters are different, not a palindrome
+    }
+
+    // Move the pointers towards the center of the word
+    start++;
+    end--;
+  }
+
+  return true; 
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // Add your own custom tests here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
